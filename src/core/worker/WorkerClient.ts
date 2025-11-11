cf4c3ff79665e59276b372da44893af053e55a62
Luke <landrewhowes@gmail.com>
2025-11-11 05:07:03 +0000

    Increase timeout for game start to 20 seconds from 5 seconds

--- src/core/worker/WorkerClient.ts
+++ src/core/worker/WorkerClient.ts
@@ -78,7 +78,7 @@ export class WorkerClient {
           this.messageHandlers.delete(messageId);
           reject(new Error("Worker initialization timeout"));
         }
-      }, 5000); // 5 second timeout
+      }, 20000); // 5 second timeout
     });
   }
 
